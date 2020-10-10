a016e2b28dc99c68349b444015aef4e993d44c0f
AbdulHalim30 <ahalim055@gmail.com>
2020-10-10 22:30:54 +0700

    The Burger Basic Version

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
+import Button from "../../UI/Button/Button";
+
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log("[OrderSummary] willUpdate");
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Adelicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>Total Price: {this.props.price.toFixed(2)}</p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
